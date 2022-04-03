f522b3b19db02b0edeb498c33d2e72a0f015967c
DESKTOP-H9O9ENN\이상지 <isj4194@naver.com>
2022-04-04 00:38:09 +0900

    Home

--- team_1/src/App.js
+++ team_1/src/App.js
@@ -1,9 +1,11 @@
-import Hello from './Hello';
+import Home from './Home';
+
 
 function App() {
   return (
     <div>
-      <Hello />
+      <Home />
+
     </div>
   );
 }
---
+++ team_1/src/Home.js
@@ -0,0 +1,9 @@
+//src/Home.js
+import './Home.css';
+
+
+function Home() {
+    return <div className="title">어서와 구스팜!</div>;
+}
+
+export default Home;
