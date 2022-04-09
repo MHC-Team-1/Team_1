41f56a94e4984f8a8bcdcfae6bca8ec1a137dc75
DESKTOP-H9O9ENN\이상지 <isj4194@naver.com>
2022-04-09 16:45:27 +0900

    Landing css edit

--- team_1/src/Landing/Landing.js
+++ team_1/src/Landing/Landing.js
@@ -4,6 +4,7 @@ import './Landing.css';
 function Home() {
     return( 
         <div>
+            <img className="icon" alt="icon" src="img/icon.jpg" />
             <div className="title">
             구스팜(로고)
             <div className='sub'>good+farm이다 (슬로건)</div>
