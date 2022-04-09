64fd223fc84c17943a8baaf0530bd20103288ac1
DESKTOP-H9O9ENN\이상지 <isj4194@naver.com>
2022-04-09 14:48:38 +0900

    Landing page edit

--- team_1/src/Landing/Landing.js
+++ team_1/src/Landing/Landing.js
@@ -1,1 +1,19 @@
-//Landing.js
+//src/Landing/Landing.js
+//import styles from './Landing/Landing.css';
+
+
+function Landing() {
+    return <div>구스팜!</div>;
+}
+
+export default Landing;
+
+// const title = document.getElementById("title");
+// console.dir(title);
+
+// title.innerText ="구스팜"
+
+
+
+// var h1 = document.getElementsByTagName('div')
+// h1[0].innerHTML= "구스팜"
