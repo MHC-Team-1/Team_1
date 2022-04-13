5700f9f67c450e405ccf39f0c89b4146d9d73042
이채은 <74644782+codms85@users.noreply.github.com>
2022-04-13 14:06:35 +0900

    update Mission3

--- team_1/src/Mission3/Mission3.js
+++ team_1/src/Mission3/Mission3.js
@@ -1,1 +1,23 @@
+import './Mission3.css';
 
+function Mission3() {
+   
+    return( 
+        <div>
+            <div className="topbar3"/>
+            <div className="comment">
+            양액으로 식물에게 영양분을 공급해야하는데<td/>어떤 농도가 좋을까?
+            </div>
+            
+            <div className="brec1">
+                <button className="cbtn1"/>
+                <button className="cbtn2"/>
+                <button className="cbtn3"/>
+            </div>
+           
+        </div>
+
+    );
+    }
+
+export default Mission3;
