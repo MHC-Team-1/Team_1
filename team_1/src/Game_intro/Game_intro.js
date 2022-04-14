347a68aa4f6997ed6a5899d8b99172ef92c44e86
DESKTOP-H9O9ENN\이상지 <isj4194@naver.com>
2022-04-14 23:26:47 +0900

    Add : BackGround, Loading_page

--- team_1/src/Game_intro/Game_intro.js
+++ team_1/src/Game_intro/Game_intro.js
@@ -5,7 +5,6 @@ function Game_intro() {
    
     return( 
             <div className='Game_intro'>
-                <div className="topbar1"/>
                 <div class="parent">
                 <img className="Talk1" alt="Talk1" src="img/Talk1.png" />
                 <img className="Talk2" alt="Talk2" src="img/Talk2.png" />
@@ -13,6 +12,10 @@ function Game_intro() {
                 <img className="Talk4" alt="Talk4" src="img/Talk4.png" />
                 <img className="Talk5" alt="Talk5" src="img/Talk5.png" />
                 <img className="Talk6" alt="Talk6" src="img/Talk6.png" />
+            </div>
+
+            <div>
+                <Link to="/Mission1"><img className="btn1" alt="YBUTTON1" src="img/YBUTTON1.png"/></Link>
             </div>
                 
         </div>
