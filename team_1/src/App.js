1d8c940ee9f0cac847092c1d887e0bba3846e204
이채은 <74644782+codms85@users.noreply.github.com>
2022-04-15 01:03:40 +0900

    build result +  fix button options

--- team_1/src/App.js
+++ team_1/src/App.js
@@ -2,7 +2,9 @@ import Landing from './Landing/Landing'
 import Game from './Game/Game'
 import Game_intro from './Game_intro/Game_intro'
 import Mission1 from './Mission1/Mission1'
+import Mission1_result from './Mission1_result/Mission1_result'
 import Mission2 from './Mission2/Mission2'
+import Mission2_result from './Mission2_result/Mission2_result'
 import Mission3 from './Mission3/Mission3'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
@@ -14,7 +16,9 @@ function App() {
         <Route path="/Game_intro" element={<Game_intro />} />
         <Route path="/Game" element={<Game />} />
         <Route path="/Mission1" element={<Mission1/>} />
+        <Route path="/Mission1_result" element={<Mission1_result/>}/>
         <Route path="/Mission2" element={<Mission2/>} />
+        <Route path="/Mission2_result" element={<Mission2_result/>}/>
         <Route path="/Mission3" element={<Mission3/>} />
       </Routes>
     </BrowserRouter>
--- team_1/src/Mission1/Mission1.js
+++ team_1/src/Mission1/Mission1.js
@@ -10,19 +10,19 @@ function Mission1() {
                     4개 중에 뭘 심어야 좋을까?
                 </div>
                 <div className="rec1">
-                    <Link to="/Mission2">
+                    <Link to="/Mission1_result">
                         <button className="abtn1"/>
                     </Link>
-                    <Link to="/Mission2">
+                    <Link to="/Mission1_result">
                         <button className="abtn2"/>
                     </Link>
                     
                 </div>
                 <div className="rec2">
-                    <Link to="/Mission2">
+                    <Link to="/Mission1_result">
                         <button className="abtn3"/>
                     </Link>
-                    <Link to="/Mission2">
+                    <Link to="/Mission1_result">
                         <button className="abtn4"/>
                     </Link>
                 </div>
