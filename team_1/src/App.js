853b2056f42099e94832fa47dc7a2d5199f128cd
DESKTOP-H9O9ENN\이상지 <isj4194@naver.com>
2022-04-16 23:21:05 +0900

    Fix : App.js, Mission1,2.js

--- team_1/src/App.js
+++ team_1/src/App.js
@@ -7,6 +7,10 @@ import Mission2 from './Mission2/Mission2'
 import Mission2Result from './Mission2Result/Mission2Result'
 import Mission3 from './Mission3/Mission3'
 import Mission3Result from './Mission3Result/Mission3Result'
+import Loading from './Loading/Loading'
+import ResultFail from './ResultFail/ResultFail'
+import ResultSuc from './ResultSuc/ResultSuc'
+import HintPage from './HintPage/HintPage'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
@@ -22,6 +26,10 @@ function App() {
         <Route path="/Mission2Result" element={<Mission2Result/>}/>
         <Route path="/Mission3" element={<Mission3/>} />
         <Route path="/Mission3Result" element={<Mission3Result/>}/>
+        <Route path="/Loading" element={<Loading/>} />
+        <Route path="/ResultFail" element={<ResultFail/>} />
+        <Route path="/ResultSuc" element={<ResultSuc/>} />
+        <Route path="/HintPage" element={<HintPage/>} />
       </Routes>
     </BrowserRouter>
   )
