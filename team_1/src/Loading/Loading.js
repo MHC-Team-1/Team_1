74d639f6829702f3823f2492af62d2ad56f3be69
DESKTOP-H9O9ENN\이상지 <isj4194@naver.com>
2022-04-18 20:52:43 +0900

    GameIntro

--- team_1/src/Loading/Loading.js
+++ team_1/src/Loading/Loading.js
@@ -2,10 +2,16 @@ import './Loading.css';
 import { Link } from "react-router-dom";
 
 function Loading() {
+
+    // let [ alert, alertSet ] = useState(true);
+    // useEffect(()=>{
+    //   let timer = setTimeout(()=>{ alertSet(false) }, 2000);
+    // });
    
     return( 
         <div className='Loading'>
             <div><img className="Loadimg" alt="Loadimg" src="img/Loadimg.png"/></div>
+                {/* alert === true */}
                        <Link to="/ResultFail"><img className="Tembtn3" alt="Tembtn" src="img/Tembtn.png"/></Link>
     </div>
 );
