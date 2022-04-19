5cfcf431c401f7d01980110fc9cdc16ce864a9a6
DESKTOP-H9O9ENN\이상지 <isj4194@naver.com>
2022-04-20 01:08:08 +0900

    Refac : ResultFail, FailRM

--- team_1/src/FailRM/FailRM.js
+++ team_1/src/FailRM/FailRM.js
@@ -1,17 +1,15 @@
-import './ResultFail.css';
+import './FailRM.css';
 import { Link } from "react-router-dom";
 
-function ResultFail() {
+function FailRM() {
    
     return( 
-        <div className='ResultFail'>
-            <img className="FailRMimg" alt="FailRMimg" src="img/FailRMimg.png"/>
+        <div className='FailRM'>
                        <Link to="/HintPage"><img className="Hint" alt="Hint" src="img/Hint.png"/></Link>
                        <Link to="/Mission1"><img className="Retry1" alt="Retry" src="img/Retry.png"/></Link>
                        <Link to="/ResultSuc"><img className="Tembtn4" alt="Tembtn" src="img/Tembtn.png"/></Link>
 
-
     </div>
 );
 }
-export default ResultFail;
+export default FailRM;
--- team_1/src/GameIntro/GameIntro.js
+++ team_1/src/GameIntro/GameIntro.js
@@ -12,11 +12,10 @@ function GameIntro() {
                 <img className="Talk4" alt="Talk4" src="img/Talk4.png" />
                 <img className="Talk5" alt="Talk5" src="img/Talk5.png" />
                 <img className="Talk6" alt="Talk6" src="img/Talk6.png" />
+                <Link to="/Mission1"><img className="Tembtn1" alt="Tembtn" src="img/Tembtn.png"/></Link>
+
             </div>
 
-            <div>
-                <Link to="/Mission1"><img className="Tembtn1" alt="Tembtn" src="img/Tembtn.png"/></Link>
-            </div>
                 
         </div>
     );
