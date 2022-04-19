5d1cabb747dc1055a54c61f7a4194749f3e1eb7c
DESKTOP-H9O9ENN\이상지 <isj4194@naver.com>
2022-04-20 04:00:19 +0900

    Add : GFInfo-

--- team_1/src/GFInfo/GFInfo.js
+++ team_1/src/GFInfo/GFInfo.js
@@ -4,8 +4,12 @@ import { Link } from 'react-router-dom'
 function GFInfo() {
   return (
     <div className='GFInfo'>
-      <img className="GFInfobg" alt="GFInfobg" src="img/GFInfobg.png" />
-      <Link to='/Event'><img className="Smtbtn" alt="Smtbtn" src="img/Smtbtn.png" />
+      <img className="GFInfoimg" alt="GFInfoimg" src="img/GFInfoimg.png" />
+      <Link to='www.naver.com'><img className="news1" alt="news1" src="img/news1.png" /></Link>
+      <img className="news2" alt="news2" src="img/news2.png" />
+      <img className="news3" alt="news3" src="img/news3.png" />
+
+      <Link to='/Event'><img className="Eventbtn" alt="Eventbtn" src="img/Eventbtn.png" />
 </Link>
   </div>
   )
