ea9c0383de34ddc94f0929633529daaf10539b6e
DESKTOP-H9O9ENN\이상지 <isj4194@naver.com>
2022-04-19 10:49:43 +0900

    Re : Fail-

--- team_1/src/App.js
+++ team_1/src/App.js
@@ -8,7 +8,7 @@ import Mission2Result from './Mission2Result/Mission2Result'
 import Mission3 from './Mission3/Mission3'
 import Mission3Result from './Mission3Result/Mission3Result'
 import Loading from './Loading/Loading'
-import ResultFail from './ResultFail/ResultFail'
+import FailRM from './FailRM/FailRM'
 import ResultSuc from './ResultSuc/ResultSuc'
 import HintPage from './HintPage/HintPage'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
@@ -27,7 +27,7 @@ function App() {
         <Route path="/Mission3" element={<Mission3/>} />
         <Route path="/Mission3Result" element={<Mission3Result/>}/>
         <Route path="/Loading" element={<Loading/>} />
-        <Route path="/ResultFail" element={<ResultFail/>} />
+        <Route path="/FailRM" element={<FailRM/>} />
         <Route path="/ResultSuc" element={<ResultSuc/>} />
         <Route path="/HintPage" element={<HintPage/>} />
       </Routes>
