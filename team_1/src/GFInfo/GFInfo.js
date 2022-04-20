dafca5f810166d54fc3b1d2b06b1313ba70c114b
DESKTOP-H9O9ENN\이상지 <isj4194@naver.com>
2022-04-20 13:54:09 +0900

    Feat : Link Connections

--- team_1/src/GFInfo/GFInfo.js
+++ team_1/src/GFInfo/GFInfo.js
@@ -1,16 +1,16 @@
 import './GFInfo.css'
-import { Link } from 'react-router-dom'
 
 function GFInfo() {
   return (
     <div className='GFInfo'>
       <img className="GFInfoimg" alt="GFInfoimg" src="img/GFInfoimg.png" />
-      <Link to='www.naver.com'><img className="news1" alt="news1" src="img/news1.png" /></Link>
-      <img className="news2" alt="news2" src="img/news2.png" />
-      <img className="news3" alt="news3" src="img/news3.png" />
+      <img onClick={() => window.open('https://www.viva100.com/main/view.php?key=20211216010004721', '_blank')} className="news1" alt="news1" src="img/news1.png" />
+      <img onClick={() => window.open('http://www.m-i.kr/news/articleView.html?idxno=911328', '_blank')} className="news2" alt="news2" src="img/news2.png" />
+      <img onClick={() => window.open('https://www.viva100.com/main/view.php?key=20220118010004822', '_blank')} className="news3" alt="news3" src="img/news3.png" />
 
-      <Link to='/Event'><img className="Eventbtn" alt="Eventbtn" src="img/Eventbtn.png" />
-</Link>
+      <img onClick={() => window.open('https://docs.google.com/forms/d/e/1FAIpQLScn-kvJwqssOMebv-0GDtkYIUSvuY81twVXNGF3KopM0nvmXA/viewform', '_blank')} className="Eventbtn" alt="Eventbtn" src="img/Eventbtn.png" />
+
+
   </div>
   )
 }
--- team_1/src/HintPage/HintPage.js
+++ team_1/src/HintPage/HintPage.js
@@ -5,7 +5,7 @@ function HintPage() {
    
     return( 
         <div className='HintPage'>
-            <Link to="/FailRM"><img className="Xbtn" alt="Xbtn" src="img/Xbtn.png"/></Link>
+            <Link to="/Mission1"><img className="Xbtn" alt="Xbtn" src="img/Xbtn.png"/></Link>
         <div>
             <img className="Hintimg" alt="Hintimg" src="img/Hintimg.png"/>
 </div>
