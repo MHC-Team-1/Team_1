4653b6baee1fbbb2020eee07b7f738d84c27c4a8
DESKTOP-H9O9ENN\이상지 <isj4194@naver.com>
2022-04-21 16:19:44 +0900

    Fix : Nextbtn

--- team_1/src/GameIntro/GameIntro.js
+++ team_1/src/GameIntro/GameIntro.js
@@ -12,7 +12,7 @@ function GameIntro() {
                 <img className="Talk4" alt="Talk4" src="img/Talk4.png" />
                 <img className="Talk5" alt="Talk5" src="img/Talk5.png" />
                 <img className="Talk6" alt="Talk6" src="img/Talk6.png" />
-                <Link to="/Mission1"><img className="Tembtn1" alt="Tembtn" src="img/Tembtn.png"/></Link>
+                <Link to="/Mission1"><img className="Nextbtn" alt="Nextbtn" src="img/Nextbtn.png"/></Link>
                 </div>
             </div>
     );
