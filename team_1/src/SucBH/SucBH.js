3e096787cbe3c17548d97d36b554d580ad244177
DESKTOP-H9O9ENN\이상지 <isj4194@naver.com>
2022-04-23 17:50:33 +0900

    Style : Suc-animation

--- team_1/src/SucBH/SucBH.js
+++ team_1/src/SucBH/SucBH.js
@@ -5,8 +5,18 @@ function SucBH() {
    
     return( 
         <div className='SucBH'>
+            <div className='Sucparent'>
+            <img className="Str1" alt="Str1" src="img/Str.png"/>
+            <img className="Str2" alt="Str2" src="img/Str.png"/>
+            <img className="Str3" alt="Str3" src="img/Str.png"/>
+            <img className="Str4" alt="Str4" src="img/Str.png"/>
+            <img className="Emoji" alt="Emoji" src="img/Emoji.png"/>
+            <img className="hun" alt="hun" src="img/hun.png"/>
+            <img className="clap" alt="clap" src="img/clap.png"/>
+            
             <img className="SucBHimg" alt="SucBHimg" src="img/SucBHimg.png"/>
 
+            </div>
             <Link to="/GFInfo"><img className="Smtbtn" alt="Smtbtn" src="img/Smtbtn.png"/></Link>
     </div>
 );
