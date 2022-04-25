c67640e762365a559bda242bc6401a7d2c1dc233
이채은 <74644782+codms85@users.noreply.github.com>
2022-04-25 23:01:33 +0900

    Set Sound & Loop

--- team_1/src/App.js
+++ team_1/src/App.js
@@ -19,31 +19,33 @@ import SucET from './SucET/SucET'
 import GFInfo from './GFInfo/GFInfo'
 import HintPage from './HintPage/HintPage'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Bgm from './Bgm/Bgm'
 
 function App() {
   return (
     <BrowserRouter>
+      <Bgm />
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/GameIntro" element={<GameIntro />} />
         <Route path="/Game" element={<Game />} />
-        <Route path="/Mission1" element={<Mission1/>} />
-        <Route path="/Mission1Result" element={<Mission1Result/>}/>
-        <Route path="/Mission2" element={<Mission2/>} />
-        <Route path="/Mission2Result" element={<Mission2Result/>}/>
-        <Route path="/Mission3" element={<Mission3/>} />
-        <Route path="/Mission3Result" element={<Mission3Result/>}/>
-        <Route path="/Loading" element={<Loading/>} />
-        <Route path="/FailRM" element={<FailRM/>} />
-        <Route path="/FailKP" element={<FailKP/>} />
-        <Route path="/FailBH" element={<FailBH/>} />
-        <Route path="/FailET" element={<FailET/>} />
-        <Route path="/SucRM" element={<SucRM/>} />
-        <Route path="/SucKP" element={<SucKP/>} />
-        <Route path="/SucBH" element={<SucBH/>} />
-        <Route path="/SucET" element={<SucET/>} />
-        <Route path="/GFInfo" element={<GFInfo/>} />
-        <Route path="/HintPage" element={<HintPage/>} />
+        <Route path="/Mission1" element={<Mission1 />} />
+        <Route path="/Mission1Result" element={<Mission1Result />} />
+        <Route path="/Mission2" element={<Mission2 />} />
+        <Route path="/Mission2Result" element={<Mission2Result />} />
+        <Route path="/Mission3" element={<Mission3 />} />
+        <Route path="/Mission3Result" element={<Mission3Result />} />
+        <Route path="/Loading" element={<Loading />} />
+        <Route path="/FailRM" element={<FailRM />} />
+        <Route path="/FailKP" element={<FailKP />} />
+        <Route path="/FailBH" element={<FailBH />} />
+        <Route path="/FailET" element={<FailET />} />
+        <Route path="/SucRM" element={<SucRM />} />
+        <Route path="/SucKP" element={<SucKP />} />
+        <Route path="/SucBH" element={<SucBH />} />
+        <Route path="/SucET" element={<SucET />} />
+        <Route path="/GFInfo" element={<GFInfo />} />
+        <Route path="/HintPage" element={<HintPage />} />
       </Routes>
     </BrowserRouter>
   )
